50929c1a67be776c8011512b6161c11ec1829869
Sam Ruhe <sar420@lehigh.edu>
2020-04-24 21:06:10 -0700

    updated deposit, using pl/sql trigger instead of jdbc

--- sar420ruhe/sar420/interfaces/items/Deposit.java
+++ sar420ruhe/sar420/interfaces/items/Deposit.java
@@ -26,30 +26,15 @@ public class Deposit {
     }
 
     private static boolean attemptDeposit(String acct_num, double deposit_amount, Connection db) {
-        try (PreparedStatement find = db.prepareStatement("SELECT minimum_balance FROM account WHERE account_number=?");
-             PreparedStatement account = db.prepareStatement("UPDATE account SET balance=balance+? WHERE account_number=?");
-             PreparedStatement savAccount = db.prepareStatement("UPDATE savings_acct SET balance=balance+? WHERE account_number=?");
-             PreparedStatement checkAccount = db.prepareStatement("UPDATE checking_acct SET balance=balance+? WHERE account_number=?");
+        try (PreparedStatement account = db.prepareStatement("UPDATE account SET balance=balance+? WHERE account_number=?");
              PreparedStatement newBalance = db.prepareStatement("SELECT balance FROM account WHERE account_number=?");
         ){
-            find.setString(1, acct_num);
-            ResultSet rs = find.executeQuery();
-            rs.next();
-            double min_bal = rs.getDouble(1);
             account.setDouble(1, deposit_amount);
             account.setString(2, acct_num);
             account.executeUpdate();
-            if (min_bal == 0.0) {
-                checkAccount.setDouble(1, deposit_amount);
-                checkAccount.setString(2, acct_num);
-                checkAccount.executeUpdate();
-            } else {
-                savAccount.setDouble(1, deposit_amount);
-                savAccount.setString(2, acct_num);
-                savAccount.executeUpdate();
-            }
+            
             newBalance.setString(1, acct_num);
-            rs = newBalance.executeQuery();
+            ResultSet rs = newBalance.executeQuery();
             rs.next();
             IOHandler.print("");
             IOHandler.printBalance(acct_num, rs.getString(1));
